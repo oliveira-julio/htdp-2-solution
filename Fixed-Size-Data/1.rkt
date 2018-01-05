c6de8bc7da353e33d0ceed4e80736e43cf3ea2a5
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-01-05 11:05:28 -0300

    Fixed-Size Data Exercise 1 distance formula

--- Fixed-Size-Data/1.rkt
+++ Fixed-Size-Data/1.rkt
@@ -2,5 +2,10 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 
-(define x 3)
-(define y 4)
+(define x 12)
+(define y 5)
+
+(sqrt (+
+       (sqr x)
+       (sqr y)))
+
