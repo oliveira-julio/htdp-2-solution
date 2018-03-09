a19fb3d6f48146830b8abffa630670b4310bf9ed
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-03-09 19:47:00 -0300

    Fixed-Size Data exercise 4

---
+++ Fixed-Size-Data/4.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |4|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+
+(define str "helloworld")
+(define i 9)
+
+(string-append
+ (substring str 0 i)
+ (substring str (add1 i) (string-length str)))
+
+
+; Which values for i are legitimate?
+; -> 0, 1, 2, ..., 9
