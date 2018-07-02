8e7bbf7f02120eb8a03d1145d492de36d71f16c6
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-02 12:27:37 -0300

    Fixed-Size Data Exercise 7

---
+++ Fixed-Size-Data/7.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |7|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
+
+(define sunny #true)
+(define friday #false)
+
+
+(or (not sunny) friday)
+
+
+;; (not sunny) => false
+;; (or false false) => false
