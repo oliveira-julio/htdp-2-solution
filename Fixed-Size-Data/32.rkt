ea156ae8c3abaadad6d3e7907a8fda870fe29289
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-06 20:31:29 -0300

    Fixed-Size Data exercise 32

---
+++ Fixed-Size-Data/32.rkt
@@ -0,0 +1,12 @@
+;; 1  - Keyboard(typing)
+;; 2  - Mouse(click)
+;; 3  - Battery(energy)
+;; 4  - Cd(read)
+;; 5  - Hd(read)
+;; 6  - Internet
+;; 7  - Time
+;; 8  - Init a program
+;; 9  - End of a computing
+;; 10 - Audio(record)
+
+
