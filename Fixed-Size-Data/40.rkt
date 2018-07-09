52f1e905d665aa3dd033b15646d2ee6f48225ff7
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-09 19:40:27 -0300

    Fixed-Size Data exercise 40

---
+++ Fixed-Size-Data/40.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |40|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+
+; WorldState -> WorldState
+; moves the car 3 pixels for every clock tick
+(check-expect (tick 20) 23)
+(check-expect (tick 78) 81)
+(define (tick ws)
+  (+ ws 3))
