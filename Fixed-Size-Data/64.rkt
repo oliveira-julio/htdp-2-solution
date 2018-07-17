2add8812d08e23b54c0310200f231203669dc892
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-17 20:50:49 -0300

    Fixed-Size Data exercise 64

---
+++ Fixed-Size-Data/64.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |64|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+
+; Posn -> Number
+; computes the distance of ap to the origin
+(check-expect (manhattan-distance (make-posn 0 5)) 5)
+(check-expect (manhattan-distance (make-posn 7 0)) 7)
+(check-expect (manhattan-distance (make-posn 3 4)) 7)
+(check-expect (manhattan-distance (make-posn 8 6)) 14)
+(check-expect (manhattan-distance (make-posn 5 12)) 17)
+(define (manhattan-distance ap)
+  (+ (posn-x ap)
+     (posn-y ap)))
