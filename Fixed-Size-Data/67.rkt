7e7a50aaf546dc4fe4ce85670d40e0430d5e10f4
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-20 16:14:27 -0300

    Fixed-Size Data exercise 67

---
+++ Fixed-Size-Data/67.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |67|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+
+(define SPEED 3)
+(define-struct balld [location direction])
+(make-balld 10 "up")
+(make-balld 10 "resting")
+(make-balld 10 "down")
