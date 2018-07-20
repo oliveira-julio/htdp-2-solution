4744b715d8b23787773df2141e91e9b3399effd6
Julio Oliveira <antoniojuliodeo@gmail.com>
2018-07-20 15:57:31 -0300

    Fixed-Size Data exercise 65

---
+++ Fixed-Size-Data/65.rkt
@@ -0,0 +1,37 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |65|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+
+; (define-struct movie [title producer year])
+; (define-struct person [name hair eyes phone])
+; (define-struct pet [name number])
+; (define-struct CD [artist title price])
+; (define-struct sweater [material size producer])
+; Write down the names of the functions (constructors, selectors, and predicates) that each introduces.
+
+; movie :
+;     - make-movie
+;     - movie-title; movie-producer; movie-year
+;     - movie?
+
+; person :
+;     - make-person
+;     - person-name; person-hair; person-eyes; person-phone
+;     - person?
+
+; pet :
+;     - make-pet
+;     - pet-name; pet-number
+;     - pet?
+
+; CD :
+;     - make-CD
+;     - CD-artist; CD-title; CD-price
+;     - CD?
+
+; sweater :
+;     - make-sweater
+;     - sweater-material; sweater-size; sweater-producer
+;     - sweater?
+
